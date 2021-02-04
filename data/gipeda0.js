0d5dea95ac464745e4dddc7e1b75a2118d521cc3
mletsios <mletsios@outlook.com>
2021-02-04 23:02:29 +0200

    Add files via upload

---
+++ data/gipeda0.js
@@ -0,0 +1,99 @@
+var json_gipeda0 = {
+    "type": "FeatureCollection",
+    "crs": {
+        "type": "name",
+        "properties": {
+            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+    },
+    "features": [
+
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Apollon Kalamarias",
+                "logo": '<img src="images/logo/kalamaria.png" class="logo">',
+                "year": 1926,
+                "site": "mgskapollon.gr",
+                "stadium": "Stadium of Kalamaria",
+                "category": "Third Division"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [22.945226164147378, 40.581419334943192]
+            }
+        },
+
+	
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "PAOK",
+                "logo": '<img src="images/logo/paok.png" class="logo">',
+                "year": 1926,
+                "site": "paokfc.gr",
+                "stadium": "Toumba Stadium",
+                "category": "First Division"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [22.972313948237769, 40.613796660849545]
+            }
+        },
+
+
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Άρης",
+                "logo": '<img src="images/logo/aris.png" class="logo">',
+                "year": 1914,
+                "site": "arisfc.com.gr",
+                "stadium": "Kleanthis Vikelidis",
+                "category": "Second Division"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [22.96951570209491, 40.600033858391825]
+            }
+        },
+
+
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Iraklis",
+                "logo": '<img src="images/logo/iraklis.png" class="logo">',
+                "year": 1908,
+                "site": "fciraklis.gr",
+                "stadium": "Kaftanzoglio Stadium",
+                "category": "First Division"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [22.967060098336894, 40.625503608998223]
+            }
+        },
+
+
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Agrotikos Asteras",
+                "logo": '<img src="images/logo/agrotikos.png" class="logo">',
+                "year": 1932,
+                "site": "paeagrotikosasteras1932.gr",
+                "stadium": "Dimitrios Kontikakis",
+                "category": "Second Division"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [22.908136910584272, 40.666981778386628]
+            }
+        }
+		
+		
+		
+		
+    ]
+}
